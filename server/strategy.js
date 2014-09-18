bb6b6d01118c623b7d99ba2d527c623c913ecc8f
Stephane Kasriel <skasriel@elance-odesk.com>
2014-09-18 13:15:59 -0700

    working on postgres

--- server/strategy.js
+++ server/strategy.js
@@ -8,14 +8,18 @@ var strategy = {};
   },
 
   function (username, password, done) {
+    console.log("In strategy, looking up user: "+username+" "+password)
     var User = require('./models/User').User;
     User.find({username: username}).success(function(user) {
       if (!user) {
+        console.log("user not found!");
         return done(null, false, { message: 'Nobody here by that name'} );
       }
       if (user.password !== password) {
+        console.log("wrong password!");
         return done(null, false, { message: 'Wrong password'} );
       }
+      console.log("correct login")
       return done(null, { username: user.username });
     });
   }
