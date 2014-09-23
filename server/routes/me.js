95f96cf57cf7de78865b3f6fa16e9b294e6593d2
Stephane Kasriel <skasriel@elance-odesk.com>
2014-09-22 22:31:29 -0700

    made updatedAt useful

--- server/routes/me.js
+++ server/routes/me.js
@@ -114,7 +114,7 @@ function uploadContact(context, callback) {
   query += ")"
   User.find({ where: query })
     .error(function(error) {
-      console.error("error retrieving users "+error);
+      console.error("error retrieving users "+error+" query = "+query);
     }).success(function(friend) {
       if (friend) {
         // this contact is already a Sapristi user. So create a Contact if there isn't one already
